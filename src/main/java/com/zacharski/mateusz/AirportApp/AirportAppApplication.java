250223f9bbd4e34531cda54e1b56cba82cff1780
Zacharsky <mateusz.zacharski1@gmail.com>
2018-11-17 11:53:08 +0100

    first commit

---
+++ src/main/java/com/zacharski/mateusz/AirportApp/AirportAppApplication.java
@@ -0,0 +1,12 @@
+package com.zacharski.mateusz.AirportApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportAppApplication.class, args);
+	}
+}
